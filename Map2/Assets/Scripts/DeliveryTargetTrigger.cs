221a23dabd295e3309b7f7829cfe4aa44a76e36c
Dongnyoung <leedo7182@naver.com>
2025-11-11 15:42:45 +0900

    feat: 1번째미션 교수님 npc추가

--- Map2/Assets/Scripts/DeliveryTargetTrigger.cs
+++ Map2/Assets/Scripts/DeliveryTargetTrigger.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class DeliveryTargetTrigger : MonoBehaviour
 {
     public QuestManager questManager;
-
+  
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
